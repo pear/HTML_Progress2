aeb55573ab22b749ff569996ebee112ce9f94138
Laurent Laville <farell@php.net>
2005-08-18 12:24:09 +0000

    changed comment shape to prevent phpDocumentor 1.2.3 wrong file assigning
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Progress2/trunk@193854 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/lite/windowstyle2.php
+++ examples/lite/windowstyle2.php
@@ -1,13 +1,15 @@
-<!--
-  Demonstration of the windowstyle feature of Progress2_Lite
-  version of Progress2 without any dependencies
-
-  @version    $Id$
-  @author     Laurent Laville <pear@laurent-laville.org>
-  @package    HTML_Progress2
-  @subpackage Examples
-  @access     public
-// -->
+<?php
+/**
+ * Demonstration of the windowstyle feature of Progress2_Lite
+ * version of Progress2 without any dependencies
+ *
+ * @version    $Id$
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @package    HTML_Progress2
+ * @subpackage Examples
+ * @access     public
+ */
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
