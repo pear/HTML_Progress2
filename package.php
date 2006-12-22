e89d26316dae46608af69622e24ef674099bfd52
Laurent Laville <farell@php.net>
2006-12-22 16:56:31 +0000

    standalone package script generator (xml 2.0 only) built with PEAR_PackageFileManager 1.6.0+
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Progress2/trunk@225578 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -1,41 +1,64 @@
 <?php
 /**
- * Make the package.xml 1.0 and 2.0 versions
- * with PEAR_PackageFileManager_Frontend_Web.
+ * HTML_Progress2 Package Script Generator
+ *
+ * Generate a new fresh version of package xml 2.0 built with PEAR_PackageFileManager 1.6.0+
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
  *
  * @category   HTML
  * @package    HTML_Progress2
  * @author     Laurent Laville <pear@laurent-laville.org>
- * @copyright  2005-2006 Laurent Laville
+ * @copyright  2006 The PHP Group
  * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    CVS: $Id$
- * @since      File available since Release 2.0.1
+ * @link       http://pear.php.net/package/HTML_Progress2
+ * @since      File available since Release 2.1.1
+ * @ignore
  */
 
-require_once 'PEAR/PackageFileManager/Frontend.php';
+require_once 'PEAR/PackageFileManager2.php';
 
-session_start();
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
-// where to find package sources
-$pkgDir = 'c:/php/pear/HTML_Progress2/package2.xml';
+$packagefile = 'c:/php/pear/HTML_Progress2/package2.xml';
 
-$web =& PEAR_PackageFileManager_Frontend::singleton('Web', $pkgDir);
-// configuration options
-$web->setOption('baseinstalldir', 'HTML');
-$web->setOption('exportcompatiblev1', true);
-$web->setOption('changelogoldtonew', false);
-$web->setOption('simpleoutput', true);
-$web->setOption('outputdirectory', 'c:/php/pear');
-$web->setOption('filelistgenerator', 'cvs');
+$options = array('filelistgenerator' => 'cvs',
+    'packagefile' => 'package2.xml',
+    'baseinstalldir' => 'HTML',
+    'simpleoutput' => true,
+    'clearcontents' => false,
+    'changelogoldtonew' => false,
+    'ignore' => array('package.php')
+    );
 
-// stop if serious error(s)
-if ($web->hasErrors('error')) {
-    $errors = $web->getErrors();
-    echo '<pre>';
-    var_dump($errors);
-    echo '</pre>';
-    die('exit on Error');
+$p2 = &PEAR_PackageFileManager2::importOptions($packagefile, $options);
+$p2->setPackageType('php');
+$p2->addRelease();
+$p2->generateContents();
+$p2->setReleaseVersion('2.1.1');
+$p2->setAPIVersion('2.1.0');
+$p2->setReleaseStability('stable');
+$p2->setAPIStability('stable');
+$p2->setNotes('* bugs
+- fixed bug #8684 : Problem with generated JavaScript
+- fixed "examples/generatorcus.php" to use new architecture with external stylesheet introduced on August 11, 2006.
+
+* news (improved code)
+- used now "className" property rather than "visibility"
+and reduced div tags count used to build a progress bar (only 10 rather than 20 )
+- apply more CS (space code indent, ...)
+- add "alt" attribute to img html-tag to be more xhtml compliant
+- JS function "hideProgress" lose its second parameter (no more needed)
+');
+
+if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
+    $p2->writePackageFile();
+} else {
+    $p2->debugPackageFile();
 }
-// run the wizard tabbed pages
-$web->run();
 ?>
