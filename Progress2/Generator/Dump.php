cfb5997672df76aedf40c68424fab7730ff4287a
Laurent Laville <farell@php.net>
2007-04-08 14:55:56 +0000

    change license from PHP 3.01 to BSD
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Progress2/trunk@233449 c90b9560-bf6c-de11-be94-00142212c4b1

--- Progress2/Generator/Dump.php
+++ Progress2/Generator/Dump.php
@@ -1,18 +1,41 @@
 <?php
 /**
- * Interactive memory debugging tool.
+ * Copyright (c) 2006-2007, Laurent Laville <pear@laurent-laville.org>
  *
- * You can display contents of :
- * - default PEAR_PackageFileManager2 class options
- * - this frontend options
- * - all forms values, defaults and validation states
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the authors nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  *
  * @category   HTML
  * @package    HTML_Progress2
  * @author     Laurent Laville <pear@laurent-laville.org>
- * @copyright  2006-2007 The PHP Group
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @copyright  2006-2007 Laurent Laville
+ * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/HTML_Progress2
  * @since      File available since Release 2.1.0
  */
 
@@ -32,6 +55,8 @@ function varDump($var)
 }
 
 /**
+ * Interactive memory debugging tool.
+ *
  * You can display contents of :
  * - default PEAR_PackageFileManager2 class options
  * - this frontend options
@@ -42,10 +67,11 @@ function varDump($var)
  * @package    HTML_Progress2
  * @author     Laurent Laville <pear@laurent-laville.org>
  * @copyright  2006-2007 Laurent Laville
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      Class available since Release 2.1.0
  */
+
 class ActionDump extends HTML_QuickForm_Action
 {
     /**
