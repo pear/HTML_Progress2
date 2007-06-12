45af0fb89e35b82dab8b894d7d12815d90135ccc
Laurent Laville <farell@php.net>
2007-06-12 10:41:33 +0000

    Package script generator : - License change from PHP 3.01 to new BSD - prepare release 2.3.0 (stable)
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Progress2/trunk@237476 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -4,17 +4,11 @@
  *
  * Generate a new fresh version of package xml 2.0 built with PEAR_PackageFileManager 1.6.0+
  *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
  * @category   HTML
  * @package    HTML_Progress2
  * @author     Laurent Laville <pear@laurent-laville.org>
- * @copyright  2006 The PHP Group
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @copyright  2006-2007 Laurent Laville
+ * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/HTML_Progress2
  * @since      File available since Release 2.1.1
@@ -33,29 +27,34 @@ $options = array('filelistgenerator' => 'cvs',
     'simpleoutput' => true,
     'clearcontents' => false,
     'changelogoldtonew' => false,
-    'ignore' => array('package.php')
+    'ignore' => array('package.php', 'CVS/', 'Thumbs.db', 'callouts/'),
+    'exceptions' => array(
+        'ChangeLog' => 'doc',
+        'NEWS' => 'doc',
+        'HOWTO_AJAX.txt' => 'doc')
     );
 
 $p2 = &PEAR_PackageFileManager2::importOptions($packagefile, $options);
 $p2->setPackageType('php');
 $p2->addRelease();
 $p2->generateContents();
-$p2->setReleaseVersion('2.2.0');
-$p2->setAPIVersion('2.2.0');
+$p2->setReleaseVersion('2.3.0');
+$p2->setAPIVersion('2.3.0');
 $p2->setReleaseStability('stable');
 $p2->setAPIStability('stable');
-$p2->setNotes('* news
-- add new function importStyle() to give ability to import an existing stylesheet
-and separate PHP code from HTML layout (with CSS)
+$p2->setNotes('* bugs
+- Fixed Doc Bug #10879 : Problem with HTML_Progress2::setCellCount()
 
 * changes
-- improved javascript custom handler for JavaDanse example related to version 2.1.1 (bug fixed #8684)
-
-* QA
-- include a new revision of User Guide (TDG) that cover all versions 2.x.x
-  see http://pear.laurent-laville.org/HTML_Progress2 for more format to download.
+- No code changes since previous release (RC1), but change license
+from PHP 3.01 to new BSD (give more freedom)
 ');
-$p2->addPackageDepWithChannel('required', 'HTML_CSS', 'pear.php.net', '1.1.2');
+
+$p2->setLicense('BSD', 'http://www.opensource.org/licenses/bsd-license.php');
+//$p2->addMaintainer('contributor', 'arpad', 'Arpad Ray', 'arpad@php.net');
+//$p2->addPackageDepWithChannel('optional', 'HTML_AJAX', 'pear.php.net', '0.5.0');
+//$p2->setExtends('HTML_Progress');
+//$p2->setPhpDep('4.3.0');
 
 if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
     $p2->writePackageFile();
